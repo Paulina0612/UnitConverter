acae9b47fb32c0eec28c11da4a626cccfb454080
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-01-06 14:30:19 +0100

    Name field in enum Unit removed.

--- app/src/main/java/unitconverter/Unit.java
+++ app/src/main/java/unitconverter/Unit.java
@@ -2,62 +2,59 @@ package unitconverter;
 
 public enum Unit {
     // LENGTH
-    METER(UnitType.LENGTH, 1, 0, "Meter", "m"),
-    KILOMETER(UnitType.LENGTH, 0.001, 0, "Kilometer", "km"),
-    CENTIMETER(UnitType.LENGTH, 100, 0, "Centimeter", "cm"),
-    MILIMETER(UnitType.LENGTH, 1000, 0, "Milimeter", "mm"),
-    MICROMETER(UnitType.LENGTH, 1000000, 0, "Micrometer", "um"),
-    NANOMETER(UnitType.LENGTH, 1000000000, 0, "Nanometer", "nm"),
+    METER(UnitType.LENGTH, 1, 0, "m"),
+    KILOMETER(UnitType.LENGTH, 0.001, 0, "km"),
+    CENTIMETER(UnitType.LENGTH, 100, 0, "cm"),
+    MILIMETER(UnitType.LENGTH, 1000, 0, "mm"),
+    MICROMETER(UnitType.LENGTH, 1000000, 0, "um"),
+    NANOMETER(UnitType.LENGTH, 1000000000, 0, "nm"),
 
     // TEMPERATURE
-    CELSIUS(UnitType.TEMPERATURE, 1, 0, "Celsius", "°C"),
-    KELVIN(UnitType.TEMPERATURE, 1, -273.15, "Kelvin", "K"),
+    CELSIUS(UnitType.TEMPERATURE, 1, 0, "°C"),
+    KELVIN(UnitType.TEMPERATURE, 1, -273.15, "K"),
 
     // AREA
-    SQUARE_METER(UnitType.AREA, 1, 0, "Square meter", "m^2"),
-    SQUARE_KILOMETER(UnitType.AREA, 0.000001, 0, "Square kilometer", "km^2"),
-    SQUARE_CENTIMETER(UnitType.AREA, 100000, 0, "Square centimeter", "cm^2"),
-    SQUARE_MILIMETER(UnitType.AREA, 10000000, 0, "Square milimeter", "mm^2"),
-    HECTARE(UnitType.AREA, 0.0001, 0, "Hectare", "ha"),
+    SQUARE_METER(UnitType.AREA, 1, 0, "m^2"),
+    SQUARE_KILOMETER(UnitType.AREA, 0.000001, 0, "km^2"),
+    SQUARE_CENTIMETER(UnitType.AREA, 100000, 0, "cm^2"),
+    SQUARE_MILIMETER(UnitType.AREA, 10000000, 0, "mm^2"),
+    HECTARE(UnitType.AREA, 0.0001, 0, "ha"),
 
     // // VOLUME
-    CUBIC_METER(UnitType.VOLUME, 1, 0, "Cubic meter", "m^3"),
-    CUBIC_KILOMETER(UnitType.VOLUME, 0.000000001, 0, "Cubic kilometer", "km^3"),
-    CUBIC_CENTIMETER(UnitType.VOLUME, 1000000, 0, "Cubic centimeter", "cm^3"),
-    CUBIC_MILIMETER(UnitType.VOLUME, 1000000000, 0, "Cubic milimeter", "mm^3"),
-    LITER(UnitType.VOLUME, 1000, 0, "Liter", "l"),
-    MILILITER(UnitType.VOLUME, 1000000, 0, "Mililiter", "ml"),
+    CUBIC_METER(UnitType.VOLUME, 1, 0, "m^3"),
+    CUBIC_KILOMETER(UnitType.VOLUME, 0.000000001, 0, "km^3"),
+    CUBIC_CENTIMETER(UnitType.VOLUME, 1000000, 0, "cm^3"),
+    CUBIC_MILIMETER(UnitType.VOLUME, 1000000000, 0, "mm^3"),
+    LITER(UnitType.VOLUME, 1000, 0, "l"),
+    MILILITER(UnitType.VOLUME, 1000000, 0, "ml"),
 
     // WEIGHT
-    GRAM(UnitType.WEIGHT, 1000, 0, "Gram", "g"),
-    KILOGRAM(UnitType.WEIGHT, 1, 0, "Kilogram", "kg"),
-    MILIGRAM(UnitType.WEIGHT, 1000000, 0, "Miligram", "mg"),
+    GRAM(UnitType.WEIGHT, 1000, 0, "g"),
+    KILOGRAM(UnitType.WEIGHT, 1, 0, "kg"),
+    MILIGRAM(UnitType.WEIGHT, 1000000, 0, "mg"),
 
     // TIME
-    SECOND(UnitType.TIME, 86400, 0, "Second", "s"),
-    MILISECOND(UnitType.TIME, 86400000, 0, "Milisecond", "ms"),
-    MINUTE(UnitType.TIME, 1440, 0, "Minute", "min"),
-    HOUR(UnitType.TIME, 24, 0, "Hour", "h"),
-    DAY(UnitType.TIME, 1, 0, "Day", "d");
+    SECOND(UnitType.TIME, 86400, 0, "s"),
+    MILISECOND(UnitType.TIME, 86400000, 0, "ms"),
+    MINUTE(UnitType.TIME, 1440, 0, "min"),
+    HOUR(UnitType.TIME, 24, 0, "hours"),
+    DAY(UnitType.TIME, 1, 0, "days");
 
     private UnitType type;
     private double factor;
     private double toAddtoBasicValue;
-    private String name;
     private String shortage;
 
-    private Unit(UnitType t, double f, double v, String n, String s){
+    private Unit(UnitType t, double f, double v, String s){
         this.type = t;
         this.factor = f;
         this.toAddtoBasicValue = v;
-        this.name = n;
         this.shortage = s;
     }
 
     public UnitType GetUnitType(){ return type; }
     public double GetFactor(){ return factor; }
     public double GetValueToAddtoBasicValue(){ return toAddtoBasicValue; }
-    public String GetName(){ return name; }
     public String GetShortage(){ return shortage; }
     
     public Unit GetBasicValue(UnitType unitType){ 
